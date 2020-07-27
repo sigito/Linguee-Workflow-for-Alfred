e72c03bb05ee8b4376f5b068682e01464f240132
Yurii Samsoniuk <yura@samsoniuk.com>
2020-07-27 20:45:33 +0200

    Convert the passed query to a canonical utf-8

--- Sources/LingueeOnAlfred/main.swift
+++ Sources/LingueeOnAlfred/main.swift
@@ -5,6 +5,10 @@ guard CommandLine.arguments.count > 1 else {
   fatalError("No query parameter provided.")
 }
 let query = CommandLine.arguments[1].trimmingCharacters(in: .whitespacesAndNewlines)
+  // Macos stores the strings in a decompose UTF8 encoding (aka. UTF8-MAC). Conversion to a conanical UTF8 string.
+  // https://developer.apple.com/library/archive/qa/qa1235/_index.html
+  // https://stackoverflow.com/questions/23219482#23226449
+  .precomposedStringWithCanonicalMapping
 
 
 let linguee = Linguee()
