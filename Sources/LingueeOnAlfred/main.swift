9796daf1a80f5c6a3df1020221e104fdf1c89b6d
Yurii Samsoniuk <yura@samsoniuk.com>
2021-02-06 11:25:07 +0100

    Added an SE number for @main support

--- Sources/LingueeOnAlfred/main.swift
+++ Sources/LingueeOnAlfred/main.swift
@@ -1,5 +1,5 @@
 import LingueeSearchWorkflow
 
-// TODO: remove when swift package supports @main.
+// TODO(SE-0294): remove when swift package supports @main.
 // We need a main.swift or otherwise swift package fails to produce an executable.
 try LingueeSearchWorkflow.main()
