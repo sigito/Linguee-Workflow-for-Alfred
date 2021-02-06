43158e9e9019d8a261fc5e98bc114d510a131d8b
Yurii Samsoniuk <yura@samsoniuk.com>
2021-02-06 12:38:06 +0100

    Fixed a typo in error item's text

--- Sources/LingueeSearchWorkflow/Error+AlfredItem.swift
+++ Sources/LingueeSearchWorkflow/Error+AlfredItem.swift
@@ -3,7 +3,7 @@ import Alfred
 extension Error {
   var alfredItem: Item {
     return .init(
-      valid: false, title: "Failed to get translations!", subtitle: "Detalis: \(self)",
+      valid: false, title: "Failed to get translations!", subtitle: "Details: \(self)",
       icon: .warning)
   }
 }
